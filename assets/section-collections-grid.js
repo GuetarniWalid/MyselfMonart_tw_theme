323c538eb5367d7c21302a94eca0d9be3e5bda8c
walid guetarni <guetarni.walid@gmail.com>
2023-01-18 23:02:43 +0100

    new section: collections grid

---
+++ assets/section-collections-grid.js
@@ -0,0 +1,10 @@
+const boxes = document.querySelectorAll('.shopify-section.collections-grid .one-collection');
+
+boxes.forEach(box => {
+  box.querySelector('.collection-title > button').addEventListener('click', () => toggleTextWrapper(box));
+  box.querySelector('.paragraph button').addEventListener('click', () => toggleTextWrapper(box));
+});
+
+function toggleTextWrapper(box) {
+  box.querySelector('.paragraph').classList.toggle('full-height');
+}
