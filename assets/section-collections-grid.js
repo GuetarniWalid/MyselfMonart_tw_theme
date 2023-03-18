be302e87f042080d270c778c4b1c8cf55d2ee979
walid guetarni <guetarni.walid@gmail.com>
2023-03-18 13:56:34 +0100

    fix bug: when the user clicks on one-collection title, the page is loaded

--- assets/section-collections-grid.js
+++ assets/section-collections-grid.js
@@ -1,10 +1,15 @@
 const boxes = document.querySelectorAll('.shopify-section.collections-grid .one-collection');
 
 boxes.forEach(box => {
-  box.querySelector('.collection-title > button')?.addEventListener('click', () => {
+  box.querySelector('.collection-title > button')?.addEventListener('click', (e) => {
+    // stop bubbling
+    e.stopPropagation();
     return toggleTextWrapper(box);
   });
   box.querySelector('.paragraph button')?.addEventListener('click', () => toggleTextWrapper(box));
+  box.querySelector('.collection-title')?.addEventListener('click', () => {
+    window.location = box.querySelector('a:first-child')?.href;
+  })
 });
 
 function toggleTextWrapper(box) {
